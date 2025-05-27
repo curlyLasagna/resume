fee3713bffa78c23ad7811480da4be6ea72e5881
Luis Dale <luis.gcodes@gmail.com>
2025-05-27 13:10:42 -0400

    Reorg. Add devenv

---
+++ devenv.nix
@@ -0,0 +1,54 @@
+{
+  pkgs,
+  lib,
+  config,
+  inputs,
+  ...
+}:
+
+{
+  # https://devenv.sh/basics/
+  env.GREET = "Welcome to Jake's template resume";
+
+  # https://devenv.sh/packages/
+  packages = [ pkgs.texlab ];
+
+  # https://devenv.sh/languages/
+  languages.texlive = {
+    enable = true;
+    base = pkgs.texliveFull;
+  };
+
+  # https://devenv.sh/processes/
+  # processes.cargo-watch.exec = "cargo-watch";
+
+  # https://devenv.sh/services/
+  # services.postgres.enable = true;
+
+  # https://devenv.sh/scripts/
+  scripts.hello.exec = ''
+    echo hello from $GREET
+  '';
+
+  enterShell = ''
+    hello
+    git --version
+  '';
+
+  # https://devenv.sh/tasks/
+  # tasks = {
+  #   "myproj:setup".exec = "mytool build";
+  #   "devenv:enterShell".after = [ "myproj:setup" ];
+  # };
+
+  # https://devenv.sh/tests/
+  enterTest = ''
+    echo "Running tests"
+    git --version | grep --color=auto "${pkgs.git.version}"
+  '';
+
+  # https://devenv.sh/git-hooks/
+  # git-hooks.hooks.shellcheck.enable = true;
+
+  # See full reference at https://devenv.sh/reference/options/
+}
